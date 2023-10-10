ee1c1ad51eca26c9e9f525826200eb840e1c109e
layeska <lisa13suarez@gmail.com>
2023-10-09 21:30:30 -0600

    Nuevo ejemplo

---
+++ app/contact/page.js
@@ -0,0 +1,11 @@
+export default function Contact() {
+    return (
+        <Head>
+            <title>Example Next.js</title>
+            <meta name='description' content='Example of a paragraph to use in Next.js'/>
+            <meta name='viewport' content='width=device-width, initial-scale=1' />
+            <meta name='robots' content='noindex, follow'/>
+            <link rel='icon' href='/favicon.ico' />
+        </Head>
+    )
+}
